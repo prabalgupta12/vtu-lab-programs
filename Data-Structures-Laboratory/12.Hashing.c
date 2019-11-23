2042ddfa18d56e11d4f327f399c254b3feaf7902
PRABAL GUPTA <44517177+prabalgupta12@users.noreply.github.com>
2019-11-24 02:24:47 +0530

    update hashing.c

--- Data-Structures-Laboratory/12.Hashing.c
+++ Data-Structures-Laboratory/12.Hashing.c
@@ -9,83 +9,88 @@ implement hashing technique to map a given key K to the address space L.
 Resolve the collision (if any) using linear probing.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct emp
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+int create(int num)
 {
-	int empno;
-	char name[20];
-	int sal;
-} EMPLOYEE;
-
-void main()
+	int key;
+	key = num % 100;
+	return key;
+}
+void display(int a[MAX])
 {
-	EMPLOYEE E;
-	FILE *fp;
-	int n, i, s = sizeof(EMPLOYEE), minusone = -1, choice, flag, index, indexcopy, id;
-	printf("Enter number of records:\n");
-	scanf("%d", &n);
-	fp = fopen("emp.txt", "w+");
-	for (i = 0; i < n; i++)
+	int i;
+	printf("\nThe hash table is ");
+	printf("\nKey/index\tEmployee id\n");
+	printf("\n");
+	for(i = 10; i < MAX; i++)
 	{
-		fwrite(&minusone, sizeof(int), 1, fp);
-		fseek(fp, s - sizeof(int), SEEK_CUR);
+		printf("%d\t\t%d\n",i,a[i]);
 	}
-	while (1)
+}
+void linear_prob(int a[MAX],int key,int num)
+{
+	int flag = 0, i, count = 10;
+	if(a[key] == -1)
 	{
-		printf("Enter\n1. Add Record\n2. Display Records\n3. Exit\n");
-		scanf("%d", &choice);
-		flag = 0;
-		switch (choice)
+		a[key] = num;
+	}
+	else
+	{
+		for(i = 0;i < MAX; i++)
 		{
-		case 1:
-			printf("Enter Employee number, Employee name and Salary:\n");
-			scanf("%d%s%d", &E.empno, E.name, &E.sal);
-			//Hash function
-			index = indexcopy = (E.empno % n);
-			fseek(fp, s * index, SEEK_SET);
-			fread(&id, sizeof(int), 1, fp);
-			//Linear Probing
-			while (id != -1)
+			if(a[i] == -1)
 			{
-				flag = 1;
-				index++;
-				if (index == n)
-					index = 0;
-				if (index == indexcopy)
-				{
-					printf("FILE FULL!!\n");
-					break;
-				}
-				fseek(fp, s * index, SEEK_SET);
-				fread(&id, sizeof(int), 1, fp);
+				count++;
 			}
-			if (!((index == indexcopy) && flag))
-			{
-				fseek(fp, s * index, SEEK_SET);
-				fwrite(&E, sizeof(EMPLOYEE), 1, fp);
-			}
-			break;
-		case 2:
-			printf("Records are:\n");
-			for (index = 0; index < n; index++)
-			{
-				fseek(fp, s * index, SEEK_SET);
-				fread(&E.empno, sizeof(int), 1, fp);
-				printf("%d\t", E.empno);
-				if (E.empno != -1)
-				{
-					fread(E.name, 20, 1, fp);
-					fread(&E.sal, sizeof(int), 1, fp);
-					printf("%s\t%d", E.name, E.sal);
-				}
-				printf("\n");
-			}
-			break;
-		case 3:
-			fclose(fp);
+		}
+		if(count == MAX)
+		{
+			printf("Hash Table is full\n");
+			display(a);
 			exit(0);
 		}
+		for(i = key + 1; i < MAX ; i++ )
+		{
+			if(a[i] == -1)
+			{
+				a[i] = num;
+				flag = 1;
+				break;
+			}
+		}
+		for(i = 10;i < key && flag == 0; i++)
+		{
+			if(a[i] == -1)
+			{
+				a[i] = num;
+				flag = 1;
+				break;
+			}
+		}
 	}
+}
+
+int main()
+{
+	int a[MAX],num,key,i;
+	int ans;
+	printf("\nCollision handling by linear probing");
+	for(i = 10; i < MAX; i++)
+	{
+		a[i] = -1;
+	}
+	do
+	{
+		printf("\nEnter the 4 digit number : ");
+		scanf("%d",&num);
+		key = create(num);
+		linear_prob(a,key,num);
+		printf("\nDo you want to continue : ");
+		scanf("%d",&ans);
+	}
+	while(ans == 1);
+	display(a);
+	return 0;
 }
